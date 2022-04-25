c034e095435c4589406a8cbca6101edcfbfae050
0z0ne-Zer0 <nik.leutin2016@outlook.com>
2022-04-25 22:42:08 +0300

    Finalized basic parser. Added colorsceme to UI

---
+++ 21CENT/Parser.cs
@@ -0,0 +1,73 @@
+﻿using AngleSharp;
+
+namespace Code
+{
+    internal class Parser
+    {
+        private static Random rn = new Random(DateTime.Now.Second);
+
+        public static async Task CatalogGet(string url, List<string> result) //Catalog parser
+        {
+            var config = Configuration.Default.WithDefaultLoader();
+            using (var context = BrowsingContext.New(config))
+            {
+                Thread.Sleep(rn.Next(250, 1000)); //Wait to bypass DDOS protection
+                using (var doc = await context.OpenAsync(url)) //Open URL
+                {
+                    var list = doc.GetElementsByClassName("result__root"); //Parse for needed content
+                    foreach (var item in list)
+                    {
+                        var str = item.Children[1].GetAttribute("href") + "?print"; //Get versions for print
+                        if (!result.Contains(str)) //Eliminate copies
+                            result.Add(str);
+                        else
+                            return;
+                    }
+                }
+            }
+        }
+
+        public static async Task CategoryGet(string url, List<string> result) //Gets categories from section index
+        {
+            var config = Configuration.Default.WithDefaultLoader();
+            using (var context = BrowsingContext.New(config))
+            {
+                Thread.Sleep(rn.Next(250, 1000));
+                using (var doc = await context.OpenAsync(url)) //Open given URL
+                {
+                    //Console.WriteLine("parsing: " + url);
+                    var list = doc.GetElementsByTagName("dt"); //Parse for <dt> tags
+                    foreach (var I in list)
+                    {
+                        var item = I.Children[0];
+                        if (item.InnerHtml.Contains("span")) //Sorting out garbage
+                            continue;
+                        var link = item.GetAttribute("href");
+                        if (String.IsNullOrEmpty(link)) //Sorting out garbage (second time)
+                            continue;
+                        result.Add(link);
+                    }
+                }
+            }
+        }
+
+        public static async Task<int> PageCountGet(string url) //Gets page count in given Catalog
+        {
+            int pageAMT = 1;
+            var config = Configuration.Default.WithDefaultLoader();
+            using (var context = BrowsingContext.New(config))
+            {
+                using (var doc = await context.OpenAsync(url)) //Open URL
+                {
+                    //Console.WriteLine("page count of " + url);
+                    var T = doc.GetElementsByClassName("cr-paging_nav"); //Get page count if exists
+                    if (T.Length == 0)
+                        return pageAMT;
+                    string tmp = T[0].TextContent.Trim().Split("/")[1].Replace(">", ""); //Get last number
+                    pageAMT = Int32.Parse(tmp);
+                }
+            }
+            return pageAMT;
+        }
+    }
+}
--- 21CENT/Program.cs
+++ 21CENT/Program.cs
@@ -1,58 +1,45 @@
-﻿using AngleSharp;
-using System.Net;
-
-namespace _21CENT
+﻿namespace Code
 {
     public class Programm
     {
-        //private static string GetNumbers(string input)
-        //{
-        //    return new string(input.Where(c => char.IsDigit(c)).ToArray());
-        //}
-
-        //private static async void PageNav(string url, int pCount)
-        //{
-        //    var config = Configuration.Default.WithDefaultLoader();
-        //    using (var context = BrowsingContext.New(config))
-        //    {
-        //        using (var doc = await context.OpenAsync(url))
-        //        {
-        //            var elem = doc.GetElementsByClassName("cr-paging_nav");
-        //            string str = GetNumbers(elem[0].TextContent.Trim()).Remove(1, 1);
-        //            pCount = int.Parse(str);
-        //        }
-        //    }
-        //}
-
-        private static async Task ListGet(string url, List<string> result)
-        {
-            var config = Configuration.Default.WithDefaultLoader();
-            using (var context = BrowsingContext.New(config))
-            {
-                using (var doc = await context.OpenAsync(url))
-                {
-                    var list = doc.GetElementsByClassName("result__root");
-                    foreach (var item in list)
-                    {
-                        var str = item.Children[1].GetAttribute("href") + "?print";
-                        if (!result.Contains(str))
-                            result.Add(str);
-                    }
-                }
-            }
-        }
-
         private static void Main()
         {
-            string path = @"..\output.txt";
-            File.Create(path);
-            var url = "https://21vek.by/mobile";
-            //int pCount = 0;
-            var result = new List<string>();
             var tasks = new List<Task>();
-            for (int i = 1; i <= 20; i++)
-                tasks.Add(ListGet(url + $"/page:{i}", result));
+            var baseCat = new List<string>()
+            {
+                {"https://www.21vek.by/kitchen/" },
+                {"https://www.21vek.by/electronics/" },
+                {"https://www.21vek.by/computers/" },
+                {"https://www.21vek.by/furniture/" },
+                {"https://www.21vek.by/house/" },
+                {"https://www.21vek.by/sanitary_engineering/" },
+                {"https://www.21vek.by/repairs/" },
+                {"https://www.21vek.by/repairing_tools/" },
+                {"https://www.21vek.by/garden/" },
+                {"https://www.21vek.by/cars/" },
+                {"https://www.21vek.by/kids/" },
+                {"https://www.21vek.by/beauty/" },
+                {"https://www.21vek.by/beauty_and_health/" },
+                {"https://www.21vek.by/sport/" },
+                {"https://www.21vek.by/tourism_activities/" },
+                {"https://www.21vek.by/pet_supplies/" },
+                {"https://www.21vek.by/business/" },
+                {"https://www.21vek.by/hobby_supplies/" }
+            };
+            //Category parse
+            var subCat = new List<string>[baseCat.Count];
+            for (int i = 0; i < subCat.Length; i++)
+                subCat[i] = new List<string>();
+            for (int i = 0; i < baseCat.Count; i++)
+                tasks.Add(Parser.CategoryGet(baseCat[i], subCat[i]));
             Task.WaitAll(tasks.ToArray());
+            //Catalog parser
+            //var url = subCat[0].ElementAt(1);
+            //var result = new List<string>();
+            //tasks = new List<Task>();
+            //for (int i = 1; i <= 100; i++)
+            //    tasks.Add(Parser.CatalogGet(url + $"page:{i}", result));
+            //Task.WaitAll(tasks.ToArray());
             Console.ReadKey();
         }
     }
