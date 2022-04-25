18f52b25d38e395d288dbf72e79d74450d1cb1d1
0z0ne-Zer0 <nik.leutin2016@outlook.com>
2022-04-25 19:06:02 +0300

    Xamarin Reload

---
+++ UI/UI/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using UI.Services;
+using UI.Views;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace UI
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ UI/UI/AppShell.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using UI.ViewModels;
+using UI.Views;
+using Xamarin.Forms;
+
+namespace UI
+{
+    public partial class AppShell : Xamarin.Forms.Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
+            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+        }
+
+        private async void OnMenuItemClicked(object sender, EventArgs e)
+        {
+            await Shell.Current.GoToAsync("//LoginPage");
+        }
+    }
+}
