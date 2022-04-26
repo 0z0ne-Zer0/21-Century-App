1ce0ef6335704f316edbdd69579466061a6c6d5d
0z0ne-Zer0 <nik.leutin2016@outlook.com>
2022-04-26 03:15:51 +0300

    Code refactoring. Cleaned-up messy code

--- 21CENT/Program.cs
+++ 21CENT/Program.cs
@@ -2,11 +2,7 @@
 {
     public class Programm
     {
-        private static void Main()
-        {
-            //Console.WriteLine("Program start: " + DateTime.Now);
-            var tasks = new List<Task>();
-            var baseCat = new List<string>()
+        private static List<string> baseCat = new List<string>()
             {
                 {"https://www.21vek.by/kitchen/" },
                 {"https://www.21vek.by/electronics/" },
@@ -27,21 +23,55 @@
                 {"https://www.21vek.by/business/" },
                 {"https://www.21vek.by/hobby_supplies/" }
             };
-            //Category parse
-            var subCat = new List<string>[baseCat.Count];
+
+        private static void CategoryParser(List<string>[] subCat) //Category parse
+        {
+            var tasks = new List<Task>();
             for (int i = 0; i < subCat.Length; i++)
                 subCat[i] = new List<string>();
             for (int i = 0; i < baseCat.Count; i++)
                 tasks.Add(Parser.CategoryGet(baseCat[i], subCat[i]));
             Task.WaitAll(tasks.ToArray());
-            //Catalog parser
-            //var url = subCat[0].ElementAt(1);
-            //var result = new List<string>();
-            //tasks = new List<Task>();
-            //for (int i = 1; i <= 100; i++)
-            //    tasks.Add(Parser.CatalogGet(url + $"page:{i}", result));
-            //Task.WaitAll(tasks.ToArray());
-            //Console.WriteLine("Program end: " + DateTime.Now);
+        }
+
+        private static void PageParser(List<string> subCat, int[] pageCount) //Page counting for URL subset
+        {
+            var tasks = new List<Task<int>>();
+            for (int i = 0; i < subCat.Count; i++)
+                tasks.Add(Parser.PageCountGet(subCat[i]));
+            Task.WaitAll(tasks.ToArray());
+            for (int i = 0; i < subCat.Count; i++)
+                pageCount[i] = tasks[i].Result;
+        }
+
+        private static void CatalogParser(List<string> subCat, int[] pageCount, List<string>[] goods) //Catalog subset parse
+        {
+            var tasks = new List<Task>();
+            for (int i = 0; i < subCat.Count; i++)
+                goods[i] = new List<string>();
+            for (int i = 0; i < subCat.Count; i++)
+            {
+                tasks.Clear();
+                Console.WriteLine($"OPID{i + 1}.\tStarting parsing {subCat[i]} now.\t" + DateTime.Now);
+                for (int j = 1; j <= pageCount[i]; j++)
+                    tasks.Add(Parser.CatalogGet(subCat[i] + $"page:{j}", goods[i]));
+                Task.WaitAll(tasks.ToArray());
+            }
+        }
+
+        private static void Main()
+        {
+            //Console.WriteLine("Program start: " + DateTime.Now);
+            int[] pageCount;
+            var subCat = new List<string>[baseCat.Count];
+            List<string>[] catalog;
+            Console.WriteLine("Starting parsing main categories.\t" + DateTime.Now);
+            CategoryParser(subCat);
+            Console.WriteLine("Starting parsing subcategories for pages.\t" + DateTime.Now);
+            PageParser(subCat[2], pageCount = new int[subCat[2].Count]);
+            Console.WriteLine("Starting parsing catalog.\t" + DateTime.Now);
+            CatalogParser(subCat[2], pageCount, catalog = new List<string>[subCat[2].Count]);
+            Console.WriteLine("Program end:\t" + DateTime.Now);
         }
     }
 }
