7020990d204533ec57b2f6ca45af2755848a37b3
0z0ne-Zer0 <nik.leutin2016@outlook.com>
2022-04-26 06:04:53 +0300

    Organizing hierarchy of resources. Added Pastel for colorcoding

--- 21CENT/Classes/ParseLibs.cs
+++ 21CENT/Classes/ParseLibs.cs
@@ -1,4 +1,6 @@
-﻿namespace Code
+﻿using Pastel;
+
+namespace Code
 {
     internal class ParseLibs
     {
@@ -24,10 +26,12 @@
                 {"https://www.21vek.by/hobby_supplies/" }
             };
 
-        public static void CategoryParser(List<string>[] subCat) //Category parse
+        private static object syncObject = new object();
+
+        public static void CategoryParser(List<List<string>> subCat) //Category parse
         {
             var tasks = new List<Task>();
-            for (int i = 0; i < subCat.Length; i++)
+            for (int i = 0; i < subCat.Count; i++)
                 subCat[i] = new List<string>();
             for (int i = 0; i < baseCat.Count; i++)
                 tasks.Add(ParserCore.CategoryGet(baseCat[i], subCat[i]));
@@ -36,16 +40,17 @@
 
         public static async Task PageParser(List<string> subCat, int[] pageCount, int o) //Page counting for URL subset
         {
-            Console.WriteLine($"OPID{o + 1}.\tStarting parsing {baseCat[o]} for page counts now.\t" + DateTime.Now);
+            Console.WriteLine(($"OPID{o + 1}.\tStarting parsing {baseCat[o]} for page counts now.\t" + DateTime.Now).Pastel("#FFFF00"));
             var tasks = new List<Task<int>>();
             for (int i = 0; i < subCat.Count; i++)
                 tasks.Add(ParserCore.PageCountGet(subCat[i]));
             await Task.WhenAll(tasks.ToArray());
+            Console.WriteLine(($"OPID{o + 1}.\tPages counted.\t" + DateTime.Now).Pastel("#00FF00"));
             for (int i = 0; i < subCat.Count; i++)
                 pageCount[i] = tasks[i].Result;
         }
 
-        public static void CatalogParser(List<string> subCat, int[] pageCount, List<string>[] goods) //Catalog subset parse
+        public static void CatalogParser(List<string> subCat, int[] pageCount, List<List<string>> goods) //Catalog subset parse
         {
             var tasks = new List<Task>();
             for (int i = 0; i < subCat.Count; i++)
@@ -53,7 +58,7 @@
             for (int i = 0; i < subCat.Count; i++)
             {
                 tasks.Clear();
-                Console.WriteLine($"OPID{i + 1}.\tStarting parsing {subCat[i]} now.\t" + DateTime.Now);
+                Console.WriteLine(($"OPID{i + 1}.\tStarting parsing {subCat[i]} now.\t" + DateTime.Now).Pastel("#FFFF00"));
                 for (int j = 1; j <= pageCount[i]; j++)
                     tasks.Add(ParserCore.CatalogGet(subCat[i] + $"page:{j}", goods[i]));
                 Task.WaitAll(tasks.ToArray());
--- 21CENT/Classes/Program.cs
+++ 21CENT/Classes/Program.cs
@@ -1,17 +1,21 @@
-﻿namespace Code
+﻿using Pastel;
+
+namespace Code
 {
     public class Programm
     {
         private static void Main()
         {
-            //Console.WriteLine("Program start: " + DateTime.Now);
+            Console.WriteLine(("Program start:\t" + DateTime.Now).Pastel("#00FF00"));
             int[][] pageCount = new int[18][];
-            var subCat = new List<string>[18];
-            List<string>[] catalog;
+            var subCat = new List<List<string>>(18);
+            for (int i = 0; i < 18; i++)
+                subCat.Add(new List<string>());
+            List<List<string>> catalog;
             Console.WriteLine("Starting parsing main categories.\t" + DateTime.Now);
             ParseLibs.CategoryParser(subCat);
             var tasks = new List<Task>();
-            for (int i = 0; i < subCat.Length; i++)
+            for (int i = 0; i < subCat.Count; i++)
                 tasks.Add(ParseLibs.PageParser(subCat[i], pageCount[i] = new int[subCat[i].Count], i));
             Task.WaitAll(tasks.ToArray());
             //for (int i = 0; i < subCat.Length; i++)
@@ -19,7 +23,7 @@
             //    Console.WriteLine($"OPID{i + 1}.\tStarting parsing catalog {baseCat[i]} now.\t" + DateTime.Now);
             //    CatalogParser(subCat[i], pageCount[i], catalog = new List<string>[subCat[i].Count]);
             //}
-            Console.WriteLine("Program end:\t" + DateTime.Now);
+            Console.WriteLine(("Program end:\t" + DateTime.Now).Pastel("#00FF00"));
             Console.ReadKey();
         }
     }
