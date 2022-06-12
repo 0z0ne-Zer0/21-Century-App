5e57563fca33402cd232d7ae36c7821f2937991f
0z0ne-Zer0 <nik.leutin2016@outlook.com>
2022-06-12 20:06:08 +0300

    Rename app

---
+++ Mobile/21 Century/21 Century/Themes/LightTheme.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace 21_Century.Themes
+{
+    [Preserve(AllMembers = true)]
+[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class LightTheme
+{
+    public LightTheme()
+    {
+        this.InitializeComponent();
+    }
+}
+}
