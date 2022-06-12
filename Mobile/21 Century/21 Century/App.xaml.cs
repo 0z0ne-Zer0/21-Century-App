3752d13a8179e2f48da74a5122db979d7630780b
0z0ne-Zer0 <nik.leutin2016@outlook.com>
2022-06-12 16:48:42 +0300

    Reloaded Xamarin as separate project

---
+++ Mobile/21 Century/21 Century/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using _21_Century.Services;
+using _21_Century.Views;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace _21_Century
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new AppShell();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Mobile/21 Century/21 Century/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using _21_Century.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace _21_Century.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
