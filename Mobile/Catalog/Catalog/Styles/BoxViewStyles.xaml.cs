cb6c3a4feb4114f05602a7a3880320188391c5ee
0z0ne-Zer0 <nik.leutin2016@outlook.com>
2022-06-13 19:31:33 +0300

    Revert "Deprecation of Xamarin"
    This reverts commit ae42271d2705cce6ae0cb1ec23886a9732220f60.

---
+++ Mobile/Catalog/Catalog/Styles/BoxViewStyles.xaml.cs
@@ -0,0 +1,13 @@
+﻿using Xamarin.Forms.Xaml;
+
+namespace Catalog.Styles
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class BoxViewStyles
+    {
+        public BoxViewStyles()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ Mobile/Catalog/Catalog/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Catalog.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace Catalog.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
