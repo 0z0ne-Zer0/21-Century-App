fe503a3054fb60457a9bc9118c055ecb652c4922
0z0ne-Zer0 <nik.leutin2016@outlook.com>
2022-06-19 16:45:12 +0300

    parser RE:loaded

---
+++ 21CENT/Models/SubCat.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _21CENT.Models
+{
+    public partial class SubCat
+    {
+        public SubCat()
+        {
+            CatalogItems = new HashSet<CatalogItem>();
+        }
+
+        public int Sid { get; set; }
+        public string? Title { get; set; }
+        public string? Link { get; set; }
+        public int? Pages { get; set; }
+        public int? Pmid { get; set; }
+
+        public virtual MainCat? Pm { get; set; }
+        public virtual ICollection<CatalogItem> CatalogItems { get; set; }
+    }
+}
